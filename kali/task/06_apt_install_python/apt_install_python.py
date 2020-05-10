02adb35b188f34b8e9fd5d50a1724799322fb4ff
elf <elf@exist.not>
2020-05-10 12:15:53 +0800

    use aliyun pypi

--- kali/task/06_apt_install_python/apt_install_python.py
+++ kali/task/06_apt_install_python/apt_install_python.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import os
 import sys
 from os import path
+import shutil
 
 class DO(object):
     order = 6
@@ -12,35 +13,32 @@ class DO(object):
     pip_conf = path.join(current_path, "pip.conf")
     script = """
 ################################################################################
-mkdir -p /root/.pip
-cp {pip_conf} /root/.pip/pip.conf
+cp {pip_conf} /root/.pip
 ################################################################################
 apt install -y python python-pip
 apt install -y python3 python3-pip
 ################################################################################
 apt install -y libpython2-dev libpython3-all-dev libssl-dev libmpfr-dev libmpc-dev
-python2 -m pip install -U pip setuptools
-# it seems some dependencies of python2 are deprecated.
-### python2 -m pip install -U pwntools
-python2 -m pip uninstall crypto pycrypto
-python2 -m pip install -U pycryptodome
-python2 -m pip install -U gmpy
-python2 -m pip install -U gmpy2
-python2 -m pip install -U pwntools
-python3 -m pip install -U pip setuptools
-python3 -m pip uninstall crypto pycrypto
-python3 -m pip install -U pycryptodome
-python3 -m pip install -U gmpy
-python3 -m pip install -U gmpy2
-python3 -m pip install -U uncompyle6
-python3 -m pip install -U pwntools
-python3 -m pip install -U requests
-python3 -m pip install -U aiohttp
-python3 -m pip install -U lxml
-python3 -m pip install -U beautifulsoup4
-python3 -m pip install -U tornado
+python2 -m pip install -U pip setuptools -i {url}
+python2 -m pip uninstall crypto pycrypto -i {url}
+python2 -m pip install -U pycryptodome -i {url}
+python2 -m pip install -U gmpy -i {url}
+python2 -m pip install -U gmpy2 -i {url}
+python2 -m pip install -U pwntools -i {url}
+python3 -m pip install -U pip setuptools -i {url}
+python3 -m pip uninstall crypto pycrypto -i {url}
+python3 -m pip install -U pycryptodome -i {url}
+python3 -m pip install -U gmpy -i {url}
+python3 -m pip install -U gmpy2 -i {url}
+python3 -m pip install -U uncompyle6 -i {url}
+python3 -m pip install -U pwntools -i {url}
+python3 -m pip install -U requests -i {url}
+python3 -m pip install -U aiohttp -i {url}
+python3 -m pip install -U lxml -i {url}
+python3 -m pip install -U beautifulsoup4 -i {url}
+python3 -m pip install -U tornado -i {url}
 ################################################################################
-    """.format(pip_conf=pip_conf)
+    """.format(url="https://mirrors.aliyun.com/pypi/simple", pip_conf=pip_conf)
 
     run = None
     print_notice = None
@@ -51,6 +49,11 @@ python3 -m pip install -U tornado
 
     @staticmethod
     def do(_):
+        if not path.exists("/root/.pip"):
+            try:
+                os.makedirs("/root/.pip", 0o755)
+            except Exception as e:
+                DO.print_error(e)
         DO.run(DO.script)
 
 
