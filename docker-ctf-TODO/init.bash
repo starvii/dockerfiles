2e54ffcb06e3d297b2c51d70a5ae9bc31793b7fd
elf <elf@exist.not>
2020-05-13 16:50:38 +0800

    not use init_user.bash

--- docker-ctf-TODO/init.bash
+++ docker-ctf-TODO/init.bash
@@ -73,8 +73,20 @@ echo -e "deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe m
 && echo -e "\numask 022\n">>/home/admin/.bashrc \
 && mkdir -p /home/ctf /home/app \
 && chown admin:admin /home/ctf /home/app \
-&& su - admin -c "bash /tmp/init_user.bash" \
-&& rm -rf /tmp/*.bash
+&& git clone --depth 1 https://github.com/pwndbg/pwndbg /home/app/pwndbg \
+&& bash /home/app/pwndbg/setup.sh \
+&& git clone --depth 1 https://github.com/scwuaptx/Pwngdb.git /home/app/pwngdb \
+&& cat /home/app/pwngdb/.gdbinit >> /home/admin/.gdbinit \
+&& git clone --depth 1 https://github.com/niklasb/libc-database.git /home/app/libc-database
+
+
+
+
+
+
+
+# && su - admin -c "bash /tmp/init_user.bash" \
+# && rm -rf /tmp/*.bash
 
 
 # LibcSearcher
